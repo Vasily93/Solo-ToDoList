c00c9c20c67d17380dc2ad56ed21dcb2239b4218
Vasily93 <markovavasily@gmail.com>
2019-05-22 09:44:19 -0700

    initial setup

---
+++ data/todos.js
@@ -0,0 +1,32 @@
+const todos = [
+    {
+        task: 'Learn JavaScript',
+        completed:true
+    },
+    {
+        task: 'Feed my Cats',
+        completed:false
+    },
+    {
+        task: 'Wash dishes',
+        completed:true
+    },
+    {
+        task: 'Takeout Trash',
+        completed:false
+    },
+    {
+        task: 'Learn Spanish',
+        completed:true
+    },
+    {
+        task: 'Make a cake',
+        completed:true
+    },
+    {
+        task: '100 push ups',
+        completed:false
+    }
+];
+
+export default todos;
---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import Component from './Component.js';
+
+class App extends Component {
+
+    render() {
+        
+    }
+}
